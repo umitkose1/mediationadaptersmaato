e91db13b9c92099fac5ba6b9e6e08568220dd486
u00569526 <umit.kose@huawei.com>
2021-05-03 16:14:11 +0300

    unused resources removed.

--- app/src/androidTest/java/com/hmsmd/huawei/ads/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/hmsmd/huawei/ads/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.hmsmd.huawei.ads
+package com.hmsmd.huawei.ads;
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.hmscl.huawei.ads", appContext.packageName)
+        assertEquals("com.hmsmd.huawei.ads", appContext.packageName)
     }
 }
--- app/src/main/java/com/hmsmd/huawei/ads/MainActivity.kt
+++ app/src/main/java/com/hmsmd/huawei/ads/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.hmsmd.huawei.ads
+package com.hmsmd.huawei.ads;
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
